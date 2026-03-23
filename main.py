9d4b238dcd5af6a7f665a2cf9caa653ff701dc9f
Andreas Hess <118386923+AndreasH4@users.noreply.github.com>
2026-03-23 23:53:10 +0100

    Update main.py
    - Split lemmatization in english and german

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet as wn
 import matplotlib.pyplot as plt
 from collections import Counter
 import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import json
 from dotenv import load_dotenv
 import numpy as np
+from HanTa import HanoverTagger as ht
+from langdetect import detect, LangDetectException
 
 
 # ---------------------------- CONSTANTS --------------------------------
 USER_AGENT = 'Projekt: Data Analysis'
-STANDARD_AUTHORS = ['unknown', 'AutoModerator']
+STANDARD_AUTHORS = ['unknown', 'AutoModerator', '[deleted]']
 N_TOP_WORDS = 20
-MUNICH_DATA_PATH = 'munich_reddit_data.csv'
+MUNICH_DATA_PATH = 'Temp/munich_reddit_data.csv'
 
 # INPUTS
 CUSTOM_STOPWORDS_PATH = 'Inputs/custom_stopwords.json'
@@ -114,33 +118,98 @@ def get_data_from_reddit(subreddit):
 
 
 
-def process(text, custom_stopwords):
+# def wntag(pttag):
+#   if pttag in ['JJ', 'JJR', 'JJS']:
+#     return wn.ADJ
+#   elif pttag in ['NN', 'NNS', 'NNP', 'NNPS']:
+#     return wn.NOUN
+#   elif pttag in ['RB', 'RBR', 'RBS']:
+#     return wn.ADV
+#   elif pttag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']:
+#     return wn.VERB
+#   return None
+
+
+
+# def lemmatize(lemmatizer,word,pos):
+#   if pos == None:
+#     return word
+#   else:
+#     return lemmatizer.lemmatize(word,pos)
+
+
+def process(text, custom_stopwords, tagger):
   """
   Additional pre-processing steps for each 'full text' in munich_data_df
   """
+  # https://textmining.wp.hs-hannover.de/Preprocessing.html
+  lan = ''
 
-  # Replace NaN values with empty strings
-  if pd.isna(text):
-    return ''
+  # Detect language
+  if pd.isna(text) or not str(text).strip():
+    lan = 'empty_text'
+  try:
+    lan = detect(text)
+  except LangDetectException:
+    lan = 'unknown'
+
+  # # Only english or german are valid languages
+  # if lan != 'en' or lan != 'de':
+  #   return ''
   
   text = text.lower()
-
   text = re.sub(r'http\S+', '', text)
-  tokens = word_tokenize(text)
+  # print(text)
 
-  # Custom and predifiend stopwords
-  custom_stopwords_english = set(custom_stopwords['english'])
-  custom_stopwords_german = set(custom_stopwords['german'])
-  stopwords_en = set(stopwords.words('english'))
-  stopwords_de = set(stopwords.words('german'))
-  all_stopwords = stopwords_en.union(stopwords_de, custom_stopwords_english, custom_stopwords_german)
+  if lan == 'en':
+    clean_tokens_en = []
+    # print('lan == en')
+    tokens = word_tokenize(text, language='english')
 
-  # 3.6 NLTK Book
-  wnl = nltk.WordNetLemmatizer()
-  clean_tokens = [wnl.lemmatize(w) for w in tokens if w.isalpha() and w not in all_stopwords]
-  clean_tokens_string = ' '.join(clean_tokens)
+    # Custom and predifiend stopwords
+    custom_stopwords_english = set(custom_stopwords['english'])
+    # custom_stopwords_german = set(custom_stopwords['german'])
+    stopwords_en = set(stopwords.words('english'))
+    # stopwords_de = set(stopwords.words('german'))
+    # all_stopwords = stopwords_en.union(stopwords_de, custom_stopwords_english, custom_stopwords_german)
+    all_stopwords_eng = stopwords_en.union(custom_stopwords_english)
 
-  return clean_tokens_string
+    # 3.6 NLTK Book
+    wnl = nltk.WordNetLemmatizer()
+    # clean_tokens_en = [wnl.lemmatize(w) for w in tokens if w.isalpha() and w not in all_stopwords_eng]
+    # tags = nltk.pos_tag(tokens)
+    # lemmata = [lemmatize(lemmatizer,word,wntag(pos)) for (word,pos) in tags]
+    lemmata = [wnl.lemmatize(w) for w in tokens if w.isalpha()]
+    clean_tokens_en = [w for w in lemmata if w not in all_stopwords_eng]
+    # print(clean_tokens_en)
+    clean_tokens_en_string = ' '.join(clean_tokens_en)
+
+    return clean_tokens_en_string
+  
+  if lan == 'de':
+    # print('lan == de')
+    clean_tokens_de = []
+    tokens = word_tokenize(text, language='german')
+    custom_stopwords_german = set(custom_stopwords['german'])
+    stopwords_de = set(stopwords.words('german'))
+    all_stopwords_de = stopwords_de.union(custom_stopwords_german)
+
+    # Lemmatise HanTa
+    tags = tagger.tag_sent(tokens)
+    # print(f'tags:\n{tags}\n')
+    for word, lemma, pos in tags:
+      # Write lemma in lower case
+      lemma_lower = lemma.lower()
+
+      if lemma_lower.isalpha() and lemma_lower not in all_stopwords_de:
+        clean_tokens_de.append(lemma_lower)
+
+    clean_tokens_de_string = ' '.join(clean_tokens_de)
+    return clean_tokens_de_string
+
+
+
+  return ''
 
 
 
@@ -245,6 +314,9 @@ def main():
   7. Plot results
   """
 
+  # Initialize HanoverTagger
+  hanover_tagger = ht.HanoverTagger('morphmodel_ger.pgz')
+
   # Retrieve secrets
   # https://www.kdnuggets.com/managing-secrets-and-api-keys-in-python-projects-env-guide
   client_id = get_secret('CLIENT_ID')
@@ -289,14 +361,43 @@ def main():
   # Replace NaN values with empty strings
   munich_data_df['title'] = munich_data_df['title'].fillna('')
   munich_data_df['text'] = munich_data_df['text'].fillna('')
+
+  # Filter out STANDARD_AUTHORS
+  # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.isin.html
+  # https://medium.com/@heyamit10/understanding-isin-with-not-in-pandas-b20099c4ed63
+  munich_data_df_rm_std_author = munich_data_df[~munich_data_df['author'].isin(STANDARD_AUTHORS)]
+  munich_data_df_rm_std_author = munich_data_df_rm_std_author.reset_index(drop=True)
+  # anzahl_übrig = len(munich_data_df_rm_std_author[munich_data_df_rm_std_author['author'].isin(STANDARD_AUTHORS)])
+  # print(f'anzahl_übrig: {anzahl_übrig}')
+
+  munich_data_df_rm_std_author['clean_text'] = munich_data_df_rm_std_author['text'].apply(process, custom_stopwords=custom_stopwords, tagger=hanover_tagger)
+  munich_data_df_rm_std_author.to_csv('Outputs/munich_data_df_rm_std_author.csv', index=False, encoding='utf-8-sig')
   
+  munich_data_df_groupby_post_id = munich_data_df_rm_std_author.groupby('post_id').agg(
+    title=pd.NamedAgg(column='title', aggfunc='first'),
+    flair=pd.NamedAgg(column='flair', aggfunc='first'),
+    clean_text=pd.NamedAgg(column='clean_text', aggfunc=join_post_id_text)
+  ).reset_index()
+
+  munich_data_df_groupby_post_id.to_csv('Outputs/munich_data_df_groupby_post_id.csv', index=False, encoding='utf-8-sig')
+
+  text_corpus_list_groupby_post_id = munich_data_df_groupby_post_id.loc[
+    munich_data_df_groupby_post_id['clean_text'].str.strip() != '',
+    'clean_text'
+  ].tolist()
+
+  print(f'\ntext_corpus_list_groupby_post_id:\n{text_corpus_list_groupby_post_id}\n')
+
+  """
+
+  # Group all post and comments by post_id
   # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.groupby.html
   # https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#the-aggregate-method
   # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.agg.html
   # https://stackoverflow.com/questions/12589481/multiple-aggregations-of-the-same-column-using-pandas-groupby-agg
   # https://medium.com/@heyamit10/understanding-groupby-and-aggregate-in-pandas-f45e524538b9
   # https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#named-aggregation
-  munich_data_df_groupby_post_id = munich_data_df.groupby('post_id').agg(
+  munich_data_df_groupby_post_id = munich_data_df_rm_std_author.groupby('post_id').agg(
     title=pd.NamedAgg(column='title', aggfunc='first'),
     flair=pd.NamedAgg(column='flair', aggfunc='first'),
     text=pd.NamedAgg(column='text', aggfunc=join_post_id_text)
@@ -304,15 +405,40 @@ def main():
 
   # print(f'\nmunich_data_df_groupby_post_id:\n{munich_data_df_groupby_post_id}\n')
 
+  # Combine title and text for theme examination
+  munich_data_df_groupby_post_id['full_text'] = munich_data_df_groupby_post_id['title'] + ' ' + munich_data_df_groupby_post_id['text']
+
+  # Additional pre-processing for each 'full text' in munich_data_df_groupby_post_id via function process()
+  # https://pandas.pydata.org/docs/reference/api/pandas.Series.apply.html
+  # munich_data_df_groupby_post_id['clean_text'] = munich_data_df_groupby_post_id['full_text'].apply(process, custom_stopwords=custom_stopwords)
+
+
+  munich_data_df_groupby_post_id['clean_text'] = munich_data_df_groupby_post_id['full_text']
+
+  # Filter out all empty or only with spaces entries out from the with data cleaning 
+  # processed 'clean text' in munich_data_df_groupby_post_id, convert it to a list as text_corpus_list
+  # https://pandas.pydata.org/docs/user_guide/indexing.html
+  # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
+  # https://pandas.pydata.org/docs/user_guide/indexing.html#setting-with-enlargement
+  # https://www.geeksforgeeks.org/pandas/python-pandas-extracting-rows-using-loc/
+  # https://medium.com/@whyamit404/understanding-pandas-loc-with-simple-examples-cd9ec8693da0
+  text_corpus_list_groupby_post_id = munich_data_df_groupby_post_id.loc[
+    munich_data_df_groupby_post_id['clean_text'].str.strip() != '',
+    'clean_text'
+  ].tolist()
+
+  print(f'\ntext_corpus_list_groupby_post_id:\n{len(text_corpus_list_groupby_post_id)}\n')
+
   munich_data_df_groupby_post_id.to_csv('Outputs/munich_data_df_groupby_post_id.csv', index=False, encoding='utf-8-sig')
 
-
   # Combine title and text for theme examination
-  munich_data_df['full_text'] = munich_data_df['title'] + ' ' + munich_data_df['text']
+  # munich_data_df['full_text'] = munich_data_df['title'] + ' ' + munich_data_df['text']
 
   # Additional pre-processing for each 'full text' in munich_data_df via function process()
   # https://pandas.pydata.org/docs/reference/api/pandas.Series.apply.html
-  munich_data_df['clean_text'] = munich_data_df['full_text'].apply(process, custom_stopwords=custom_stopwords)
+  # munich_data_df['clean_text'] = munich_data_df['full_text'].apply(process, custom_stopwords=custom_stopwords)
+  # munich_data_df['clean_text'] = munich_data_df_groupby_post_id['full_text'].apply(process, custom_stopwords=custom_stopwords)
+
 
   # Filter out all empty or only with spaces entries out from the with data cleaning 
   # processed 'clean text' in munich_data_df, convert it to a list as text_corpus_list
@@ -321,10 +447,11 @@ def main():
   # https://pandas.pydata.org/docs/user_guide/indexing.html#setting-with-enlargement
   # https://www.geeksforgeeks.org/pandas/python-pandas-extracting-rows-using-loc/
   # https://medium.com/@whyamit404/understanding-pandas-loc-with-simple-examples-cd9ec8693da0
-  text_corpus_list = munich_data_df.loc[
-    munich_data_df['clean_text'].str.strip() != '',
-    'clean_text'
-  ].tolist()
+  # text_corpus_list = munich_data_df.loc[
+  #   munich_data_df['clean_text'].str.strip() != '',
+  #   'clean_text'
+  # ].tolist()
+  # print(f'\ntext_corpus_list_standard:\n{len(text_corpus_list)}\n')
 
   # output_debug = munich_data_df[['full_text', 'clean_text']]
   # output_debug.to_csv(OUTPUT_DEBUG_PATH, index=False, encoding='utf-8-sig')
@@ -332,21 +459,21 @@ def main():
 
   # ------------------- Extract most active users and used flairs ----------------------------------
   # Drop out authors with no value
-  authors = munich_data_df['author'].dropna()
+  authors = munich_data_df_rm_std_author['author'].dropna()
 
   # Filter authors, which are not contained in STANDARD_AUTHORS
   # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.isin.html
   # https://medium.com/@heyamit10/understanding-isin-with-not-in-pandas-b20099c4ed63
-  filtered_authors = authors[~authors.isin(STANDARD_AUTHORS)]
+  # filtered_authors = authors[~authors.isin(STANDARD_AUTHORS)]
   
   # Return number of unique authors
   # https://pandas.pydata.org/docs/reference/api/pandas.Series.nunique.html
-  nunique_filtered_authors = filtered_authors.nunique()
+  nunique_filtered_authors = authors.nunique()
   print(f'Anzahl unterschiedlicher Nutzer im Reddit-Datensatz:\n{nunique_filtered_authors}\n')
   
   # Return all unique authors 
   # https://pandas.pydata.org/docs/reference/api/pandas.Series.unique.html
-  unique_filtered_authors = filtered_authors.unique()
+  unique_filtered_authors = authors.unique()
 
   # Save all unique authors to csv file
   # https://www.geeksforgeeks.org/python/python-save-list-to-csv/
@@ -358,14 +485,14 @@ def main():
   # https://pandas.pydata.org/docs/reference/api/pandas.Series.value_counts.html
   # https://pandas.pydata.org/docs/reference/api/pandas.Series.head.html
   # https://pandas.pydata.org/docs/reference/api/pandas.Series.reset_index.html
-  most_active_authors_df = filtered_authors.value_counts().head(10).reset_index()
+  most_active_authors_df = authors.value_counts().head(10).reset_index()
 
   # Define column headings and write to csv file
   most_active_authors_df.columns = ['Nutzer', 'Anzahl Posts/Kommentare']
   most_active_authors_df.to_csv(MOST_ACTIVE_AUTHORS_CSV_PATH, index=False, encoding='utf-8-sig')
 
   # Extract most used flairs
-  flairs = munich_data_df['flair'].dropna()
+  flairs = munich_data_df_rm_std_author['flair'].dropna()
   nunique_flairs = flairs.nunique()
   print(f'Anzahl unterschiedlicher Flairs im Reddit-Datensatz:\n{nunique_flairs}\n')
   unique_flairs = flairs.unique()
@@ -389,13 +516,15 @@ def main():
   # Converts posts/comments from text_corpus_list to a matrix of TF-IDF featurs, where words are weighted
   # Matrix contains importance of specific words relative to whode post/comment collection from text_corpus_list 
   vectorizer = TfidfVectorizer(use_idf=True, # Enable inverse-document-frequency (IDF)
-                               min_df=5, # Ignore terms with document frequency lower than 5
-                               max_df=0.90, # Ignore terms with document frequency higher than 90%
+                               min_df=5, # Ignore terms with document frequency lower than 3
+                               max_df=0.98, # Ignore terms with document frequency higher than 90%
                                smooth_idf=True # Prevents zero divisions by adding one to document frequencies by default
                                )
+
   # Learn voocabulary from text_corpus_list and return sparse matrix 
   # of (n_samples, n_features) Tf-idf-weighted document-term matrix
-  model = vectorizer.fit_transform(text_corpus_list)
+  # model = vectorizer.fit_transform(text_corpus_list)
+  model = vectorizer.fit_transform(text_corpus_list_groupby_post_id)
 
   # Get output feature names for transformation
   feature_names = vectorizer.get_feature_names_out()
@@ -418,7 +547,7 @@ def main():
   print('End Training')
   
   # Format top words with each weight from each topic as dictionary
-  lda_results = top_words_in_json_format(lda_model, feature_names, N_TOP_WORDS)
+  lda_results = top_words_in_dict_format(lda_model, feature_names, N_TOP_WORDS)
 
   # Write results from LDA as json to lda_results.json
   # https://www.geeksforgeeks.org/python/write-multiple-variables-to-a-file-using-python/
@@ -431,7 +560,7 @@ def main():
   plot_bar_chart(most_active_authors_df, 'Anzahl Posts/Kommentare', 'Nutzer')
   plot_bar_chart(most_common_flairs_df, 'Anzahl Flairs', 'Flairs')
   plot_top_words(lda_model, feature_names, N_TOP_WORDS)
-
+  """
   return munich_data_df
 
 
